4b1a69709d94b4c3aa9b1c4ea4db853f2aa8d5bf
Lanboku <m.hirayama0129@gmail.com>
2021-01-08 07:03:30 +0900

    【Update】標準入力から取得した値を出力

--- rust/the_rust_programming_language/guessing_game/src/main.rs
+++ rust/the_rust_programming_language/guessing_game/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // Rust の変数は基本的には immutable だが mut を付与することで可変の変数となる
+    // :: で静的メソッドの呼び出し
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess) // ＆ で変数の参照
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
