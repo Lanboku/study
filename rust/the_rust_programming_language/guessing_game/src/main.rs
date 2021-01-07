668bec6b5a008df9478a8ee96dd46540e4f41497
Lanboku <m.hirayama0129@gmail.com>
2021-01-08 08:10:57 +0900

    【Update】ループの追加

--- rust/the_rust_programming_language/guessing_game/src/main.rs
+++ rust/the_rust_programming_language/guessing_game/src/main.rs
@@ -11,25 +11,33 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // Rust の変数は基本的には immutable だが mut を付与することで可変の変数となる
-    // :: で静的メソッドの呼び出し
-    let mut guess = String::new();
+        // Rust の変数は基本的には immutable だが mut を付与することで可変の変数となる
+        // :: で静的メソッドの呼び出し
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess) // ＆ で変数の参照
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess) // ＆ で変数の参照
+            .expect("Failed to read line");
 
-    // shadowing で同じ変数名を再定義できる
-    // trim で 前後の空白（改行含む）を除去して parse で u32 に変換
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // shadowing で同じ変数名を再定義できる
+        // trim で 前後の空白（改行含む）を除去して parse で u32 に変換
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, // _ は包括値
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too samll!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too samll!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
