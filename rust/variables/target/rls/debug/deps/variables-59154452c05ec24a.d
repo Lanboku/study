4a40b161d98e600b5a542a2178bca246ec63886d
Lanboku <m.hirayama0129@gmail.com>
2021-01-13 06:23:42 +0900

    【Add】cargo new --bin variables

---
+++ rust/variables/target/rls/debug/deps/variables-59154452c05ec24a.d
@@ -0,0 +1,5 @@
+/workspaces/study/rust/variables/target/rls/debug/deps/variables-59154452c05ec24a.rmeta: src/main.rs
+
+/workspaces/study/rust/variables/target/rls/debug/deps/variables-59154452c05ec24a.d: src/main.rs
+
+src/main.rs:
---
+++ rust/variables/target/rls/debug/deps/variables-c579acb3d8955f38.d
@@ -0,0 +1,5 @@
+/workspaces/study/rust/variables/target/rls/debug/deps/variables-c579acb3d8955f38.rmeta: src/main.rs
+
+/workspaces/study/rust/variables/target/rls/debug/deps/variables-c579acb3d8955f38.d: src/main.rs
+
+src/main.rs:
