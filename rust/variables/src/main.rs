683e6e3f6a3418b60c30889b45adf4b3b3f1754e
Lanboku <m.hirayama0129@gmail.com>
2021-01-14 06:45:04 +0900

    【Clean】Warning除去

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -7,10 +7,14 @@ fn main() {
     let y = 5;
     let y = y + 1;
     let y = y * 2;
+    println!("The value of y is: {}", y);
 
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     let tup2 = (500, 6.4, 1);
     let (x, y, z) = tup2;
+
+    println!("The value of y is: {}", x);
     println!("The value of y is: {}", y);
+    println!("The value of y is: {}", z);
     println!("The value of tup.0 is: {}", tup.0);
 }
