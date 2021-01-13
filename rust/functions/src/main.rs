9105dd7707bc16326f5e2b0de4abc7da463adc93
Lanboku <m.hirayama0129@gmail.com>
2021-01-14 06:57:17 +0900

    【Update】ブロックで式を定義する

--- rust/functions/src/main.rs
+++ rust/functions/src/main.rs
@@ -2,6 +2,14 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5);
+    let y = {
+        let x = 3;
+        // 式は終端にセミコロンを含まない、セミコロンをつけると文になる
+        // 文は値を返さない
+        x + 1
+    };
+
+    println!("The value of y is: {}", y)
 }
 
 fn another_function(x: i32) {
