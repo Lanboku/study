ebd7312a8c574291b542f08df7505a907f143e06
Lanboku <m.hirayama0129@gmail.com>
2021-02-17 22:00:47 +0900

    【Clean】コメントの追加とコード整理

--- vue/todo-app-sample/index.js
+++ vue/todo-app-sample/index.js
@@ -1,3 +1,4 @@
+const localStorageKeyName = "tasks";
 const vm = new Vue({
   el: "#app",
   data: {
@@ -5,21 +6,28 @@ const vm = new Vue({
     taskList: [],
   },
   mounted: function () {
+    // 初回ロード時に既存のタスクを読み込む
     this.loadTaskList();
   },
   methods: {
+    // ローカルストレージに保存したタスクリストを取得
     loadTaskList: function () {
-      this.taskList = JSON.parse(localStorage.getItem("tasks"));
+      this.taskList = JSON.parse(localStorage.getItem(localStorageKeyName));
     },
+    // タスクリストをローカルストレージに保存
     saveTaskList: function () {
-      localStorage.setItem("tasks", JSON.stringify(this.taskList));
+      localStorage.setItem(localStorageKeyName, JSON.stringify(this.taskList));
     },
+    // フォームに入力されたタスクをリストに追加
     addTask: function () {
+      // フォームが空だった場合は何もしない
+      if (this.newTask === "") return;
       this.taskList.push({
         title: this.newTask,
         isChecked: false,
       });
 
+      // タスクの追加後に入力フォームを初期化
       this.newTask = "";
       this.saveTaskList();
     },
